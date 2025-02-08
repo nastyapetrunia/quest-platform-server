5f91c79b2458ce075dd77a78f0665878f22bce6b
nastyapetrunia <nastyagr217@gmail.com>
2025-02-08 22:51:19 +0100

    temporary cors

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_socketio import SocketIO
 from flask_restx import Api
+from flask_cors import CORS
 
 from src.routes.auth_routes import auth_ns
 from src.routes.user_routes import user_ns
@@ -8,6 +9,8 @@ from src.routes.general_routes import general_ns
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
+
+CORS(app, resources={r"/*": {"origins": "*"}}, allow_headers="*")
 
 api = Api(app, version='1.0', title='MVP Quests API',
           description='API for MVP Quests services.',
